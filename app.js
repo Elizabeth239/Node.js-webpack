f0ccd7347c4a1cd2a805cbc8e9267bdac87b128c
Elizabeth239 <bogomoleclisa@gmail.com>
2021-05-26 19:04:09 +0300

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const parser=require("./parser.js")
 let eventName="parser";
 const Emitter=require("events");
 let emitter= new Emitter();
-let myText=`2020-02-03T13:50:33.166Z A 2\n2020-02-03T13:51:33.166Z B 1\n2020-02-03T13:52:33.166Z C 6\n2020-02-03T13:53:33.166Z B 8\n2020-02-03T13:54:33.166Z B 5`;
+let myText=`2020-02-03T13:50:33.166Z A 3\n2020-02-03T13:51:33.166Z B 1\n2020-02-03T13:52:33.166Z C 6\n2020-02-03T13:53:33.166Z B 8\n2020-02-03T13:54:33.166Z B 5`;
 emitter.on(eventName, function(myText){
 	parser.Parser(myText)
 });
